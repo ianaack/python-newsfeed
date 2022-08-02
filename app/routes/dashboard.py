01e4db383972fa0433b723a7de8db72baab7153e
ianaack <ianaack@gmail.com>
2022-08-02 13:51:40 -0400

    Login and Signup authentication implemented

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint("dashboard", __name__, url_prefix="/dashboard")
+bp = Blueprint("dashboard", __name__, url_prefix="/dashboard/")
 
 
 @bp.route("/")
